9440bbfdafbb697755df39b28882dbb0fad16760
Daniel Shiffman <daniel@shiffman.net>
2017-01-03 14:39:14 -0500

    scrapping bugfix and rename data file

--- scrapers_node/facebook_page_posts.js
+++ scrapers_node/facebook_page_posts.js
@@ -4,7 +4,7 @@ var FB = require('fb');
 var config = require('./config');
 FB.setAccessToken(config.token);
 
-var name = 'barackobama';
+var name = 'WhiteHouse';
 var page = name + '/feed';
 
 var allposts = [];
@@ -18,8 +18,8 @@ function gotPage(res) {
     return;
   } else {
     var posts = res.data;
-    var json = JSON.stringify(res, null, 2);
-    fs.writeFileSync('res.json', json);
+    // var json = JSON.stringify(res, null, 2);
+    // fs.writeFileSync('res.json', json);
     
     console.log('got ' + posts.length + ' post');
     console.log('1 Created_time: ' + posts[0].created_time);
@@ -31,20 +31,33 @@ function gotPage(res) {
     }
 
     // url for next page
-    var previous = res.paging.previous.match(/since=\d+/)[0];
-    var next = res.paging.next.match(/until=\d+/)[0];
+    var previous = res.paging.previous.match(/since=\d+/);//[0];
+    var next = res.paging.next.match(/until=\d+/);//[0];
+
+    // Is there an next?
+    if (!next) {
+      writeData();
+      return;
+    }
+    previous = previous[0];
+    next = next[0];
+
     // What's the page id
     var prev = previous.substring(6, previous.length);
     var nxt = next.substring(6, next.length);
     console.log(prev,nxt);
+
     if (prev == nxt) {
-      // we're done
-      var json = JSON.stringify(allposts, null, 2);
-      fs.writeFileSync(name + '.json', json);
-      console.log('finished: ' + name + '.json');
+      writeData();
     } else {
       // Keep going
       FB.api(page + '/?' + next, 'get', gotPage);
     }
   }
 };
+
+function writeData() {
+  var json = JSON.stringify(allposts, null, 2);
+  fs.writeFileSync(name + '.json', json);
+  console.log('finished: ' + name + '.json');
+}
