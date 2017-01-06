6317438a3f2d3603a2e25253c952a32dcf1f0c0c
Daniel Shiffman <daniel@shiffman.net>
2017-01-06 16:33:28 -0500

    comments for tweets example

--- examples/P5/TweetsByMonth/sketch.js
+++ examples/P5/TweetsByMonth/sketch.js
@@ -1,6 +1,17 @@
+// Obamathon
+// https://github.com/ITPNYU/Obamathon
+// YouTube video tutorial: https://youtu.be/UrznYJltZrU
+
+
+// All data
 var potus;
+
+// An object with a property for each month
 var counts = {};
 
+// List of words to ignore
+// This is silly and a better strategy would be to load from a text files
+// or use an algorithm like TF-IDF to pick out unique words
 var ignore = {
   "the": 'true',
   "to": 'true',
@@ -13,30 +24,43 @@ var ignore = {
   "our": 'true'
 }
 
+// Load the data
 function preload() {
   potus = loadJSON('flotus.json');
 
 }
 
+
 function setup() {
   createCanvas(600, 400);
   var tweets = potus.tweets;
+  // Look at every tweet
   for (var i = 0; i < tweets.length; i++) {
     var date = new Date(tweets[i].timestamp);
+    // Determine month and year
     var month = date.getMonth();
     var year = date.getFullYear();
     var key = month + '/' + year;
+
+    // Increase the count by 1 for each tweet
     if (counts.hasOwnProperty(key)) {
       counts[key].total++;
     } else {
       counts[key] = {
         total: 1,
+        // Initialize an object to hold a words table for each month
         words: {}
       };
     }
+
+    // Tweet text
     var txt = tweets[i].text;
+    // Split up the words
+    // The regex could be improved to deal with apostrophes and other
+    // non word data.
     var words = txt.split(/\W+/);
 
+    // Count each time a word appears
     for (var j = 0; j < words.length; j++) {
       var word = words[j].toLowerCase();
       if (word.length > 0) {
@@ -51,10 +75,11 @@ function setup() {
   }
   background(0);
 
-  console.log(counts);
+  // Reverse the order
   var months = Object.keys(counts);
   months.reverse();
 
+  // Normalize all the data by finding the maximum number
   var maxtweets = 0;
   for (var i = 0; i < months.length; i++) {
     var month = months[i];
@@ -65,13 +90,17 @@ function setup() {
   }
   var w = width / months.length;
 
+  // Graph the data
   for (var i = 0; i < months.length; i++) {
     var month = months[i];
     var num = counts[month].total;
+    // Height of bar is number of tweets
     var h = map(num, 0, maxtweets, 0, 300);
     fill(200);
     rect(i * w, height - h, w - 1, h);
 
+    // Find the word with the largest counts
+    // This could be improved.
     var wordCounts = counts[month].words;
     var words = Object.keys(wordCounts);
     var biggest = 0;
@@ -83,12 +112,10 @@ function setup() {
         biggestWord = word;
       }
     }
-    console.log(month, biggestWord, biggest);
+    // Draw the word
     fill(255);
     text(biggestWord, i * w, height - h - 5);
 
   }
-
-  console.log(months);
 
 }
